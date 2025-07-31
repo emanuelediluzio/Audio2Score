8fc87195ed31dbfcd45e739479e253a42fb0dc5d
emanuelediluzio <48859533+emanuelediluzio@users.noreply.github.com>
2025-07-31 17:08:01 +0200

    Update png_to_icns.sh

--- png_to_icns.sh
+++ png_to_icns.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Usage: ./png_to_icns.sh icons/source_icon.png
+# Usage: ./png_to_icns.sh path/to/source_icon.png
+if [ -z "$1" ]; then
+  echo "Usage: ./png_to_icns.sh path/to/icon.png"
+  exit 1
+fi
 
-INPUT=$1
-NAME="app"
+mkdir -p icon.iconset
+sips -z 16 16     "$1" --out icon.iconset/icon_16x16.png
+sips -z 32 32     "$1" --out icon.iconset/icon_16x16@2x.png
+sips -z 32 32     "$1" --out icon.iconset/icon_32x32.png
+sips -z 64 64     "$1" --out icon.iconset/icon_32x32@2x.png
+sips -z 128 128   "$1" --out icon.iconset/icon_128x128.png
+sips -z 256 256   "$1" --out icon.iconset/icon_128x128@2x.png
+sips -z 256 256   "$1" --out icon.iconset/icon_256x256.png
+sips -z 512 512   "$1" --out icon.iconset/icon_256x256@2x.png
+sips -z 512 512   "$1" --out icon.iconset/icon_512x512.png
+cp "$1" icon.iconset/icon_512x512@2x.png
+iconutil -c icns icon.iconset -o icons/app.icns
+rm -r icon.iconset
 
-mkdir -p icons/$NAME.iconset
-
-sips -z 16 16     $INPUT --out icons/$NAME.iconset/icon_16x16.png
-sips -z 32 32     $INPUT --out icons/$NAME.iconset/icon_16x16@2x.png
-sips -z 32 32     $INPUT --out icons/$NAME.iconset/icon_32x32.png
-sips -z 64 64     $INPUT --out icons/$NAME.iconset/icon_32x32@2x.png
-sips -z 128 128   $INPUT --out icons/$NAME.iconset/icon_128x128.png
-sips -z 256 256   $INPUT --out icons/$NAME.iconset/icon_128x128@2x.png
-sips -z 256 256   $INPUT --out icons/$NAME.iconset/icon_256x256.png
-sips -z 512 512   $INPUT --out icons/$NAME.iconset/icon_256x256@2x.png
-cp $INPUT icons/$NAME.iconset/icon_512x512.png
-cp $INPUT icons/$NAME.iconset/icon_512x512@2x.png
-
-iconutil -c icns icons/$NAME.iconset -o icons/$NAME.icns
-rm -rf icons/$NAME.iconset
-
-echo "✅ Icona .icns generata in icons/$NAME.icns"
+echo "Icona creata: icons/app.icns"
