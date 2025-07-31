ee1378cf5edeb7c33a0f42e946297948ebcf03f1
emanuelediluzio <48859533+emanuelediluzio@users.noreply.github.com>
2025-07-31 17:07:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
 APP = ['trascrizione_gui.py']
+DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'iconfile': 'icons/app.icns',
-    'packages': ['music21', 'basic_pitch', 'pretty_midi', 'soundfile', 'librosa', 'torch', 'torchaudio'],
+    'packages': ['music21', 'basic_pitch', 'torchaudio', 'librosa', 'tkinter'],
 }
 
 setup(
     app=APP,
-    name="Audio2Score",
+    name='Audio2Score',
+    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
